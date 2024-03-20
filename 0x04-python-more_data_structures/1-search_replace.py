db86d2a4f10e6ad9c3b0f4f3010dcb5930e6d49f
mokaya100 <mokayageoffrey844@gmail.com>
2024-03-20 08:11:14 -0700

    one

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-
-
 def search_replace(my_list, search, replace):
-    """
-        A function that replaces all occurrences
-            of an element by another in a new list
-                """
-                    new_list = []
-                        for element in my_list:
-                                if element == search:
-                                            new_list.append(replace)
-                                                    else:
-                                                                new_list.append(element)
-                                                                    return new_list
+    def find_search(element):
+            return element if element != search else replace
+                return list(map(find_search, my_list))
