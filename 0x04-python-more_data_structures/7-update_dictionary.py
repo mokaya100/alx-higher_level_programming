deba9d28180211c21e81320b53835cb68f750345
mokaya100 <mokayageoffrey844@gmail.com>
2024-03-20 10:37:30 -0700

    yes

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-        return a_dictionary
+    """ replaces or adds key/value in a dictionary."""
+        a_dictionary[key] = value
+            return a_dictionary
+
+
+
+
+
+
+
+
+
