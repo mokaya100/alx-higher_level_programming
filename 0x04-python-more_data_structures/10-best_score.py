ee962dbdd263a183a835cc315ca19e875c64ee4d
mokaya100 <mokayageoffrey844@gmail.com>
2024-03-20 12:41:12 -0700

    none

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if my_dict is None or my_dict == {}:
-            return None
-                biggest = max(my_dict.values())
-                    for key, value in my_dict.items():
-                            if value is biggest:
-                                        return key
+def best_score(a_dictionary):
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
