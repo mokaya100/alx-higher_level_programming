99c859031680f57448bd43c34fd6be5560790875
mokaya100 <mokayageoffrey844@gmail.com>
2024-03-20 13:01:04 -0700

    yes

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
-
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str):
+    if not roman_string or type(roman_string) != str:
             return 0
                 total = 0
-                    num = 0
-                        digits = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-                            for r in reversed(roman_string):
-                                    num = digits[r]
-                                            total += num if total < num * 5 else -num
-                                                return total
+                    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+                        for roman in reversed(roman_string):
+                                arabic = digits[roman]
+                                        total += arabic if total < arabic * 5 else -arabic
+                                            return total
