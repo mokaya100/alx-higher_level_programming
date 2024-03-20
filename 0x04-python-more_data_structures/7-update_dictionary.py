47e181a69d1244b11eda06adc8c5f4b412a08cd5
mokaya100 <mokayageoffrey844@gmail.com>
2024-03-20 08:42:55 -0700

    message

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-def update_dictionary(my_dict, key, value):
-    my_dict[key] = value
-        return (my_dict)
+def update_dictionary(a_dictionary, key, value):
+    """replace a value in a dictionary
+
+    Args:
+            a_dictionary: the dictionary
+                    key: the key
+                            value: the value
+
+    Returns:
+        """
+            a_dictionary[key] = value
+                return (a_dictionary)
