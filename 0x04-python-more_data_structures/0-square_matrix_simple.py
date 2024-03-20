7120db7560c0208288fd5e442722882306a0c035
mokaya100 <mokayageoffrey844@gmail.com>
2024-03-20 08:07:54 -0700

    clear

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
-
 def square_matrix_simple(matrix=[]):
-        """
-    wordA function that computes the square
-    value of all integers of a matrix.
-    """
- new -matrix = [ ]                                                                                                      for cool in  matrix :                                                                                                  result = list (map(lambdax :x**2,col))                                                                                 new -matrix. append (result)                                                                                           return new - matrix
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]
