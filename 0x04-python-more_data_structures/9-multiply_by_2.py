72fbcab64361a5f88d38fc27639aaede3bd46795
mokaya100 <mokayageoffrey844@gmail.com>
2024-03-20 12:27:18 -0700

    true

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    tmp_dict = my_dict.copy()
-        for x in tmp_dict.keys():
-                tmp_dict[x] *= 2
-                    return (tmp_dict)
+def multiply_by_2(a_dictionary):
+    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
