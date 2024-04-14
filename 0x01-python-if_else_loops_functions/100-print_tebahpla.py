ffe16c6741e8bb187e817a6e4bbea4f68f65b3a9
mokaya100 <mokayageoffrey844@gmail.com>
2024-04-14 04:34:35 -0700

    yes

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-digit = abs(number) % 10
-if number < 0:
-    digit = -digit
-    print(f"Last digit of {number:d} is {digit:d} and is ", end="")
-    if digit > 5:
-        print("greater than 5")
-        elif digit == 0:
-            print("0")
-            else:
-                print("less than 6 and not 0")
+# Author - Godswill Kalu
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
