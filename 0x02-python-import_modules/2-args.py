fd2edf3079374b7f004d29a63a7683323a1aaf45
mokaya100 <mokayageoffrey844@gmail.com>
2024-04-14 05:28:34 -0700

    yes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-from variable_load_5 import a
-
-
-def find_var():
-    print(a)
-
 
 if __name__ == "__main__":
-    find_var()
+    """Print the number of and list of arguments."""
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
