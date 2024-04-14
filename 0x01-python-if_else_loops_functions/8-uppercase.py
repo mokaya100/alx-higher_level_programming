acc836f2b9f0f4be2ee88caa4c3b86d9d993e089
mokaya100 <mokayageoffrey844@gmail.com>
2024-04-14 04:17:49 -0700

    true

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,8 @@
 
 def uppercase(str):
     """Print a string in uppercase."""
-        for c in str:
-                if ord(c) >= 97 and ord(c) <= 122:
-                            c = chr(ord(c) - 32)
-                                    print("{}".format(c), end="")
-                                        print("")
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
